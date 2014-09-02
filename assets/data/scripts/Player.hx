6acd572ddddb51e4f652ab28507adc0ee904b76c
NicoM1 <nico.may99@gmail.com>
2014-09-02 11:50:24 -0700

    basic player setup

---
+++ assets/data/scripts/Player.hx
@@ -0,0 +1,22 @@
+package ;
+
+import ice.entity.Entity;
+import flixel.FlxG;
+
+class Player
+{
+	//#
+	var owner:Entity;
+	//#
+	
+	public function init() 
+	{
+		owner.makeGraphic(12, 24, 0xff0000ff);
+		owner.x = FlxG.width / 2 - owner.width / 2;
+		owner.y = FlxG.height - owner.height;
+	}
+	
+	public function update()
+	{
+	}
+}
