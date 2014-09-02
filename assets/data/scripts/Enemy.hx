b46fbff8256f236a7ffcc04f78e51592c9dfa11f
NicoM1 <nico.may99@gmail.com>
2014-09-02 12:05:45 -0700

    test crouching, for #4

---
+++ assets/data/scripts/Enemy.hx
@@ -0,0 +1,28 @@
+package ;
+
+import flixel.FlxG;
+import ice.entity.Entity;
+import flixel.util.FlxRandom;
+
+class Enemy
+{
+	//#
+	var owner:Entity;
+	//#
+	
+	public function init()
+	{
+		owner.makeGraphic(12, 24, 0xffff0000);
+		
+		owner.y = FlxG.height - owner.height;
+		
+		if (FlxRandom.sign() > 0)
+		{
+			owner.x = FlxG.width / 2 + owner.width;
+		}
+		else
+		{
+			owner.x = FlxG.width / 2 - owner.width;
+		}
+	}
+}
--- assets/data/scripts/Player.hx
+++ assets/data/scripts/Player.hx
@@ -9,6 +9,10 @@ class Player
 	var owner:Entity;
 	//#
 	
+	var target:Entity;
+	
+	var crouching:Bool = false;
+	
 	public function init() 
 	{
 		owner.makeGraphic(12, 24, 0xff0000ff);
@@ -18,6 +22,19 @@ class Player
 	
 	public function update()
 	{
+		if (FlxG.keys.pressed.S)
+		{
+			crouching = true;
+			owner.y = FlxG.height - owner.height + 8;
+		}
+		else
+		{
+			if (crouching)
+			{
+				crouching = false;
+				owner.y = FlxG.height - owner.height;
+			}
+		}
 		if (FlxG.keys.justPressed.SPACE)
 		{
 			FlxG.camera.shake(0.01, 0.2);
